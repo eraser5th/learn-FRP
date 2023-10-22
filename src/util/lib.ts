ec9f29ca4d063a9ac0d8957a98b261f638bf2806
eraser5th <83107074+eraser5th@users.noreply.github.com>
2023-10-23 01:48:39 +0900

    fix range function

--- src/util/lib.ts
+++ src/util/lib.ts
@@ -1,2 +1,2 @@
-export const range = (s: number, e: number, step: number = 1) =>
-  new Array(e - s).map((_, i) => i * step + s);
+export const range = (s: number, e: number, step: number = 1): number[] =>
+  new Array(e - s).fill(0).map((_, i) => i * step + s);
