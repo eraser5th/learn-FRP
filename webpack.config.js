061cfc95171bdb43330637c8290de43eb2f7154d
eraser5th <83107074+eraser5th@users.noreply.github.com>
2023-10-23 00:56:50 +0900

    Delete ex3 and ex4

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,31 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  mode: 'development',
+  mode: "development",
   entry: {
-    main: './src/index.ts',
-    ex1: './src/ex1/index.ts',
-    ex2: './src/ex2/index.ts',
-    ex3: './src/ex3/index.ts',
-    ex4: './src/ex4/index.ts',
-    airline: './src/airline/index.ts',
+    main: "./src/index.ts",
+    ex1: "./src/ex1/index.ts",
+    ex2: "./src/ex2/index.ts",
+    airline: "./src/airline/index.ts",
   },
   output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
   },
   resolve: {
-    extensions: ['.ts', '.js'],
+    extensions: [".ts", ".js"],
   },
   module: {
     rules: [
       {
         test: /\.ts$/,
-        loader: 'ts-loader',
+        loader: "ts-loader",
       },
     ],
   },
   devServer: {
     static: {
-      directory: path.join(__dirname, 'dist'),
+      directory: path.join(__dirname, "dist"),
       watch: true,
     },
   },
