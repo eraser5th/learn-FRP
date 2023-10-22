2e0d009a1a90d78d36c7a2eb29b39f68e106b341
eraser5th <83107074+eraser5th@users.noreply.github.com>
2023-10-23 00:50:24 +0900

    Update airline

--- src/airline/SDateField.ts
+++ src/airline/SDateField.ts
@@ -1,61 +1,65 @@
-import { Cell } from 'sodiumjs';
-import { ViewItem } from '../util/ViewItem';
-import { SSelect } from './SSelect';
+import { Cell } from "sodiumjs";
+import { ViewItem } from "../util/ViewItem";
+import { SSelect } from "./SSelect";
+import { range } from "lodash";
 
-const months = [
-  { label: 'Jan', value: 'Jan' },
-  { label: 'Feb', value: 'Feb' },
-  { label: 'Mar', value: 'Mar' },
-  { label: 'Apr', value: 'Apr' },
-  { label: 'May', value: 'May' },
-  { label: 'Jun', value: 'Jun' },
-  { label: 'Jul', value: 'Jul' },
-  { label: 'Aug', value: 'Aug' },
-  { label: 'Sep', value: 'Sep' },
-  { label: 'Oct', value: 'Oct' },
-  { label: 'Nov', value: 'Nov' },
-  { label: 'Dec', value: 'Dec' },
-] as const;
+const createOption = <S>(s: S) => ({ label: s, value: s });
+
+const months = (
+  [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ] as const
+).map(createOption);
 
 const monthIndex = (month: string) => {
   switch (month) {
-    case 'Jan':
+    case "Jan":
       return 0;
-    case 'Feb':
+    case "Feb":
       return 1;
-    case 'Mar':
+    case "Mar":
       return 2;
-    case 'Apr':
+    case "Apr":
       return 3;
-    case 'May':
+    case "May":
       return 4;
-    case 'Jun':
+    case "Jun":
       return 5;
-    case 'Jul':
+    case "Jul":
       return 6;
-    case 'Aug':
+    case "Aug":
       return 7;
-    case 'Sep':
+    case "Sep":
       return 8;
-    case 'Oct':
+    case "Oct":
       return 9;
-    case 'Nov':
+    case "Nov":
       return 10;
-    case 'Dec':
+    case "Dec":
       return 11;
     default:
       return 0;
   }
 };
 
-const years = [
-  { label: '2023', value: '2023' },
-  { label: '2024', value: '2024' },
-  { label: '2025', value: '2025' },
-  { label: '2026', value: '2026' },
-  { label: '2027', value: '2027' },
-  { label: '2028', value: '2028' },
-] as const;
+const years = range(2023, 2040)
+  .map((n) => n.toString())
+  .map(createOption);
+
+const dates = range(1, 31)
+  .map((n) => n.toString())
+  .map(createOption);
 
 export class SDateField implements ViewItem<HTMLDivElement> {
   private element: HTMLDivElement;
@@ -63,19 +67,22 @@ export class SDateField implements ViewItem<HTMLDivElement> {
   public value: Cell<Date>;
 
   constructor() {
-    this.element = document.createElement('div');
+    this.element = document.createElement("div");
 
-    const year = new SSelect([...years], '2023');
-    const month = new SSelect([...months], 'Jan');
+    const year = new SSelect([...years], "2023");
+    const month = new SSelect([...months], "Jan");
+    const date = new SSelect([...dates], "1");
 
-    this.value = year.value.lift(month.value, (oy, om) => {
+    this.value = year.value.lift3(month.value, date.value, (oy, om, od) => {
       const date = new Date();
+      date.setTime(0);
       date.setFullYear(parseInt(oy));
       date.setMonth(monthIndex(om));
+      date.setDate(parseInt(od));
       return date;
     });
 
-    this.appendChild(year, month);
+    this.appendChild(year, month, date);
   }
 
   getElement() {
